35e05c5d7cc834bdf90c226060bdc77367a03af7
Silvia Phungky <Silvia.Phungky@mandirisek.co.id>
2023-02-06 17:00:59 +0700

    move to page

--- pages/_document.js
+++ pages/_document.js
@@ -7,17 +7,6 @@ export default function Document() {
         <meta name="description" content="Test ANTD in SSR" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <style
-          id="holderStyle"
-          dangerouslySetInnerHTML={{
-            __html: `
-      /* https://github.com/ant-design/ant-design/issues/16037#issuecomment-483140458 */
-      /* Not only antd, but also any other style if you want to use ssr. */
-      *, *::before, *::after {
-        transition: none!important;
-      }`,
-          }}
-        />
       </Head>
       <body>
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -15,14 +15,23 @@ export default function Home({ darkMode, setDarkMode }) {
     <>
       <Head>
         <title>Test ANTD in SSR</title>
+        <style
+          id="holderStyle"
+          dangerouslySetInnerHTML={{
+            __html: `
+      /* https://github.com/ant-design/ant-design/issues/16037#issuecomment-483140458 */
+      /* Not only antd, but also any other style if you want to use ssr. */
+      *, *::before, *::after {
+        transition: none!important;
+      }`,
+          }}
+        />
       </Head>
-      {/* <main className={styles.main}> */}
       <Card>
         <Button type="primary" onClick={() => setDarkMode(!darkMode)}>
           change mode
         </Button>
       </Card>
-      {/* </main> */}
     </>
   )
 }

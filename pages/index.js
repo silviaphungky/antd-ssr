ba7ba729d347cf86fd4fb218f5973ee2e2542bb9
Silvia Phungky <Silvia.Phungky@mandirisek.co.id>
2023-02-06 17:15:35 +0700

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
-import {
-  Card,
-  Button,
-  ConfigProvider,
-  theme as antTheme,
-  Typography,
-} from 'antd'
+import { Card, Button, theme as antTheme } from 'antd'
 
 const inter = Inter({ subsets: ['latin'] })
 

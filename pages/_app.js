5a843b7b88fc621193fec83ad9bdaa2c85f2fb7a
Silvia Phungky <Silvia.Phungky@mandirisek.co.id>
2023-02-06 15:26:28 +0700

    add button

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import '@/styles/globals.css'
+import {
+  Card,
+  Button,
+  ConfigProvider,
+  theme as antTheme,
+  Typography,
+} from 'antd'
+import { useState } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [darkMode, setDarkMode] = useState(false)
+
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: darkMode
+          ? antTheme.darkAlgorithm
+          : antTheme.defaultAlgorithm,
+      }}
+    >
+      <Component {...pageProps} darkMode={darkMode} setDarkMode={setDarkMode} />
+    </ConfigProvider>
+  )
 }

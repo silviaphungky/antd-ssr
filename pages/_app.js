2f7eb4529e0d69ef04c74838e0259ed440f53148
Silvia Phungky <Silvia.Phungky@mandirisek.co.id>
2023-02-07 10:54:36 +0700

    holder style

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ConfigProvider, theme as antTheme } from 'antd'
 import { useState, useEffect } from 'react'
 import 'antd/dist/reset.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   const [darkMode, setDarkMode] = useState(false)
@@ -9,19 +10,41 @@ export default function App({ Component, pageProps }) {
    * resolve FUOC on first load
    * https://github.com/ant-design/ant-design/issues/16037#issuecomment-509024637
    *  */
-  useEffect(() => {
-    document.getElementById('preventFlashOfUnstyledContent')?.remove()
-  }, [])
+  if (typeof window !== 'undefined') {
+    window.onload = () => {
+      document.getElementById('holderStyle').remove()
+    }
+  }
 
   return (
-    <ConfigProvider
-      theme={{
-        algorithm: darkMode
-          ? antTheme.darkAlgorithm
-          : antTheme.defaultAlgorithm,
-      }}
-    >
-      <Component {...pageProps} darkMode={darkMode} setDarkMode={setDarkMode} />
-    </ConfigProvider>
+    <>
+      <Head>
+        <style
+          id="holderStyle"
+          dangerouslySetInnerHTML={{
+            __html: `
+      /* https://github.com/ant-design/ant-design/issues/16037#issuecomment-483140458 */
+      /* Not only antd, but also any other style if you want to use ssr. */
+      *, *::before, *::after {
+        transition: none!important;
+      }
+    `,
+          }}
+        />
+      </Head>
+      <ConfigProvider
+        theme={{
+          algorithm: darkMode
+            ? antTheme.darkAlgorithm
+            : antTheme.defaultAlgorithm,
+        }}
+      >
+        <Component
+          {...pageProps}
+          darkMode={darkMode}
+          setDarkMode={setDarkMode}
+        />
+      </ConfigProvider>
+    </>
   )
 }

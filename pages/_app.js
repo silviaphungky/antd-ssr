73c0148c6367e9cd731c126751e3f3dc303f6813
Silvia Phungky <Silvia.Phungky@mandirisek.co.id>
2023-02-07 10:46:31 +0700

    import antd css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import { ConfigProvider, theme as antTheme } from 'antd'
-import Head from 'next/head'
 import { useState, useEffect } from 'react'
-// import styles from '@/styles/Home.module.css'
 import 'antd/dist/reset.css'
 
 export default function App({ Component, pageProps }) {
@@ -11,31 +9,19 @@ export default function App({ Component, pageProps }) {
    * resolve FUOC on first load
    * https://github.com/ant-design/ant-design/issues/16037#issuecomment-509024637
    *  */
-  // useEffect(() => {
-  //   document.getElementById('preventFlashOfUnstyledContent')?.remove()
-  // }, [])
-
-  if (typeof window !== 'undefined') {
-    window.onload = () => {
-      document.getElementById('holderStyle').remove()
-    }
-  }
-  console.log('ssr sisil')
+  useEffect(() => {
+    document.getElementById('preventFlashOfUnstyledContent')?.remove()
+  }, [])
 
   return (
-    <>
-      <Head>
-        <style
-          id="holderStyle"
-          dangerouslySetInnerHTML={{
-            __html: `
-      *, *::before, *::after {
-        transition: none !important;
-      }`,
-          }}
-        />
-      </Head>
+    <ConfigProvider
+      theme={{
+        algorithm: darkMode
+          ? antTheme.darkAlgorithm
+          : antTheme.defaultAlgorithm,
+      }}
+    >
       <Component {...pageProps} darkMode={darkMode} setDarkMode={setDarkMode} />
-    </>
+    </ConfigProvider>
   )
 }
